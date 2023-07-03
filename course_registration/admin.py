132509ba6efec53b36b9deefc19a8679293a91d8
nacht-falter <81268159+nacht-falter@users.noreply.github.com>
2023-07-04 00:39:20 +0200

    Create Course model and register it in admin.py

--- course_registration/admin.py
+++ course_registration/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "registration_status",
+        "description",
+        "start_date",
+        "end_date",
+        "course_fee",
+    )
+    search_fields = ["title", "description"]
+    list_filter = ("registration_status",)
+    prepopulated_fields = {"slug": ("title",)}
--- course_registration/models.py
+++ course_registration/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+REGISTRATION_STATUS = ((0, "Closed"), (1, "Open"))
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    registration_status = models.IntegerField(
+        choices=REGISTRATION_STATUS, default=0
+    )
+    course_fee = models.IntegerField()
+
+    class Meta:
+        ordering = ["start_date"]
+
+    def __str__(self):
+        return self.title
