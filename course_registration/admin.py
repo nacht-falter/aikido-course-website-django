ba7320cb2d7fa659d4c3111e17fe4fbf00cc8f5c
nacht-falter <81268159+nacht-falter@users.noreply.github.com>
2023-07-04 21:08:56 +0200

    Add action for course duplication to CourseAdmin()

--- course_registration/admin.py
+++ course_registration/admin.py
@@ -15,3 +15,27 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields = ["title", "description"]
     list_filter = ("registration_status",)
     prepopulated_fields = {"slug": ("title",)}
+    actions = [
+        "duplicate_selected_courses",
+    ]
+
+    def duplicate_selected_courses(self, request, queryset):
+        """Action for duplicating existing courses"""
+        for course in queryset:
+            new_title = f"Copy of {course.title}"
+            new_slug = f"copy-of-{course.slug}"
+            counter = 2
+            while Course.objects.filter(title=new_title).exists():
+                new_title = f"Copy {counter} of {course.title}"
+                new_slug = f"copy-{counter}-of-{course.slug}"
+                counter += 1
+
+            Course.objects.create(
+                title=new_title,
+                slug=new_slug,
+                description=course.description,
+                registration_status=0,
+                start_date=course.start_date,
+                end_date=course.end_date,
+                course_fee=course.course_fee
+            )
