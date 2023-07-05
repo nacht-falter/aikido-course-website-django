e660e4ea2ecab41f0b5e339cb9b898197748fbfd
nacht-falter <81268159+nacht-falter@users.noreply.github.com>
2023-07-05 09:35:34 +0200

    Create CourseList view, add templates and urls - Create CourseList view in views.py - Add base.html and course_list.html templates - Add 'courses/' to urls.py

--- course_registration/views.py
+++ course_registration/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Course
 
-# Create your views here.
+
+class CourseList(generic.ListView):
+    model = Course
+    queryset = Course.objects.all()
+    template_name = 'course_list.html'
