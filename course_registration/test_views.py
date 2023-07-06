c081f6d34c8ee684bb3db032be20b5f5a884f915
nacht-falter <81268159+nacht-falter@users.noreply.github.com>
2023-07-06 11:53:57 +0200

    Create tests for CourseRegistration view in test_views.py

--- course_registration/test_views.py
+++ course_registration/test_views.py
@@ -1,6 +1,7 @@
 from datetime import date, timedelta
-from django.test import TestCase, Client
-from .models import Course
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Course, CourseRegistration
 
 
 class CourseListTest(TestCase):
@@ -21,9 +22,65 @@ class CourseListTest(TestCase):
     def test_course_list_view(self):
         print("\ntest_course_list_view")
         courses = Course.objects.all()
-        client = Client()
-        response = client.get("/courses/")
+        response = self.client.get("/courses/")
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "course_list.html")
         self.assertEqual(len(courses), self.number_of_courses)
+
+
+class RegisterCourseTest(TestCase):
+    """Tests for RegisterCourse view"""
+
+    def setUp(self):
+        self.course = Course.objects.create(
+            title="Test course",
+            slug="test-course",
+            start_date=date.today(),
+            end_date=date.today() + timedelta(days=1),
+            registration_status=(1),
+            course_fee=50,
+        )
+        # Test user login:
+        # https://docs.djangoproject.com/en/3.2/topics/testing/tools/#django.test.Client.force_login
+        self.user = User.objects.create_user(
+            username="test-user", password="testpassword"
+        )
+        self.client.force_login(self.user)
+
+    def test_get_registration_form(self):
+        print("\ntest_get_registration_form")
+        response = self.client.get("/courses/register/test-course/")
+        self.assertTemplateUsed(response, "register_course.html")
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_is_registered_for_course(self):
+        print("\ntest_user_is_registered_for_course")
+        CourseRegistration.objects.create(
+            user=self.user,
+            course=self.course,
+            final_fee=50,
+            payment_status=0,
+            accept_terms=True,
+            exam=False,
+            comment="Test comment",
+        )
+        # Test Redirect: https://stackoverflow.com/a/49353888
+        response = self.client.get("/courses/register/test-course/")
+        self.assertRedirects(response, "/courses/", 302, 200)
+
+    def test_post_valid_registration_form(self):
+        print("\ntest_post_valid_registration_form")
+        response = self.client.post(
+            "/courses/register/test-course/", {"final_fee": 50, "accept_terms": True, "exam": False}
+        )
+        self.assertRedirects(response, "/courses/", 302, 200)
+        course_registration = CourseRegistration.objects.all()
+        self.assertEqual(len(course_registration), 1)
+
+    def test_post_invalid_registration_form(self):
+        print("\ntest_post_invalid_registration_form")
+        response = self.client.post("/courses/register/test-course/")
+        self.assertRedirects(response, "/courses/", 302, 200)
+        course_registration = CourseRegistration.objects.all()
+        self.assertEqual(len(course_registration), 0)
