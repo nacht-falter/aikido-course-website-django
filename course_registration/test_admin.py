ad21c157251149ac19e3789a1ba4dbb457266ef0
nacht-falter <81268159+nacht-falter@users.noreply.github.com>
2023-07-07 13:39:47 +0200

    Add tests for get_course_registration_count method - Update admin.py to include test for the method - Update requirements.txt and .gitignore after   installing coverage

--- course_registration/test_admin.py
+++ course_registration/test_admin.py
@@ -1,18 +1,23 @@
+from datetime import date, timedelta
+
 from django.contrib.admin.sites import AdminSite
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test import RequestFactory
-from datetime import date, timedelta
-from .models import Course
+
+from .models import Course, CourseRegistration
 from .admin import CourseAdmin
 
 
-# Testing Django admin instructions from: https://www.argpar.se/posts/programming/testing-django-admin
+# Testing Django admin instructions from:
+# https://www.argpar.se/posts/programming/testing-django-admin
 request_factory = RequestFactory()
 request = request_factory.get("/admin")
 
 
 class TestCourseAdmin(TestCase):
     """Tests for the CourseAdmin model"""
+    print("Tests for CourseAdmin model")
 
     def setUp(self):
         site = AdminSite()
@@ -24,6 +29,19 @@ class TestCourseAdmin(TestCase):
             end_date=date.today() + timedelta(days=1),
             registration_status=(0),
             course_fee=50,
+        )
+        self.user = User.objects.create_user(
+            username="test-user", password="testpassword"
+        )
+        self.client.force_login(self.user)
+        self.registration = CourseRegistration.objects.create(
+            user=self.user,
+            course=self.course,
+            final_fee=50,
+            payment_status=0,
+            accept_terms=True,
+            exam=False,
+            comment="Test comment",
         )
 
     def test_duplicate_course_action(self):
@@ -38,3 +56,11 @@ class TestCourseAdmin(TestCase):
         self.assertTrue(second_copy)
         self.assertEqual(first_copy.slug, "copy-of-test-course")
         self.assertEqual(second_copy.slug, "copy-2-of-test-course")
+
+    def test_get_course_registration_count(self):
+        print("\ntest_get_course_registration_count")
+        registrations = CourseRegistration.objects.filter(course=self.course)
+        registration_count = self.admin.get_course_registration_count(
+            self.course
+        )
+        self.assertEqual(len(registrations), registration_count)
