7328bfe484c43a03990b03b5e3034999c05462b9
nacht-falter <81268159+nacht-falter@users.noreply.github.com>
2023-07-20 15:36:52 +0200

    Update index.html to include badge for open registrations - Add badge element to index.html for courses with open registration_status - Add main.js to static/js/ with script to auto open accordion item - Update course_list.html to work with script

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+// Open accordion item if course is targeted from url
+if (document.getElementById("accordion")) {
+// Get Course id from url:
+// https://stackoverflow.com/a/1036564
+  const id = location.hash.replace("#", "");
+  console.log(id);
+  let item = document.getElementById(`button-${id}`);
+  item.click();
+}
