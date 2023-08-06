ac91491d14983b2fbc7a43a3edb387eb1e0f9d86
nacht-falter <johannes.bernet@posteo.de>
2023-08-06 14:08:36 +0200

    Update tests in test_views for modified RegisterCourse view

--- course_registration/test_views.py
+++ course_registration/test_views.py
@@ -74,6 +74,17 @@ class RegisterCourseTest(TestCase):
         self.assertTemplateUsed(response, "register_course.html")
         self.assertEqual(response.status_code, 200)
 
+        self.user_profile.delete()
+        response = self.client.get("/courses/register/test-course/")
+        self.assertRedirects(response, "/user/profile/", 302, 200)
+
+        # Test messages: https://stackoverflow.com/a/46865530
+        messages = [m.message for m in get_messages(response.wsgi_request)]
+        self.assertIn(
+            "Please create a user profile and try again.",
+            messages,
+        )
+
     def test_user_is_registered_for_course(self):
         print("\ntest_user_is_registered_for_course")
         CourseRegistration.objects.create(
