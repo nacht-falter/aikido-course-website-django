a0d78aa5453a9cc5190a6da7855daee90b0f0d56
nacht-falter <johannes.bernet@posteo.de>
2024-07-31 00:31:09 +0200

    update settings.py to get allowed hosts, static root, media root from environment vars

--- danbw_website/settings.py
+++ danbw_website/settings.py
@@ -38,11 +38,7 @@ DEBUG = development
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
-ALLOWED_HOSTS = [
-    "aikido-course-website-django-ddffe52bc952.herokuapp.com",
-    "dev.danbw.de",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
@@ -217,11 +213,10 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.environ.get("STATIC_ROOT")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
