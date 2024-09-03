51f55d760126c66b80256d6c85acdbffb55d4225
Johannes Bernet <jofb@posteo.de>
2024-09-04 00:15:42 +0200

    Add logging to settings.py

--- danbw_website/settings.py
+++ danbw_website/settings.py
@@ -183,6 +183,37 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOG_FILE = os.path.join(BASE_DIR, "server.log")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": LOG_FILE,
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
