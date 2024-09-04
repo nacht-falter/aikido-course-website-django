91715f2c2b79f4d6e29c0e4f5b512b400d05cb3f
Johannes Bernet <jofb@posteo.de>
2024-09-04 17:53:18 +0200

    Use regex for user urls

--- users/urls.py
+++ users/urls.py
@@ -1,35 +1,26 @@
-from django.urls import path
-
-from .views import CustomPasswordChangeView
+from django.urls import re_path
 
 from . import views
+from .views import CustomPasswordChangeView
 
 urlpatterns = [
-    path(
-        "user/profile/",
-        views.UserProfileView.as_view(),
-        name="userprofile",
-    ),
-    path(
-        "user/profile/update/",
-        views.UpdateUserProfile.as_view(),
-        name="update_userprofile",
-    ),
-    path(
-        "user/deactivate/",
-        views.DeactivateUser.as_view(),
-        name="deactivate_user",
-    ),
-    path(
-        "user/update-grade/",
-        views.UpdateGrade.as_view(),
-        name="update_grade",
-    ),
-    # Override default allauth password redirect url
-    # https://stackoverflow.com/a/56599071
-    path(
-        "accounts/password/change/",
-        CustomPasswordChangeView.as_view(),
-        name="account_change_password",
-    ),
+    # User profile view
+    re_path(r"^user/profile/?$",
+            views.UserProfileView.as_view(), name="userprofile"),
+
+    # Update user profile view
+    re_path(r"^user/profile/update/?$",
+            views.UpdateUserProfile.as_view(), name="update_userprofile"),
+
+    # Deactivate user view
+    re_path(r"^user/deactivate/?$", views.DeactivateUser.as_view(),
+            name="deactivate_user"),
+
+    # Update grade view
+    re_path(r"^user/update-grade/?$",
+            views.UpdateGrade.as_view(), name="update_grade"),
+
+    # Custom password change view
+    re_path(r"^accounts/password/change/?$",
+            CustomPasswordChangeView.as_view(), name="account_change_password"),
 ]
