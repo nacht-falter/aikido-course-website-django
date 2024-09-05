7cae72257652169f292f7e3776f9f8748e65b05a
Johannes Bernet <jofb@posteo.de>
2024-09-05 18:36:49 +0200

    Fix database settings

--- danbw_website/settings.py
+++ danbw_website/settings.py
@@ -152,24 +152,17 @@ THUMBNAIL_BASEDIR = "thumbnails"
 WSGI_APPLICATION = "danbw_website.wsgi.application"
 
 
-# if development:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         "default": dj_database_url.parse(os.environ.get("DATABASE_URL", ""))
-#     }
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if development:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL", ""))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
