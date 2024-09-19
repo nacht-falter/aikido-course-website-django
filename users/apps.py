2469caa0838e06c84e36c6ef21699a66e55ce76f
Johannes Bernet <jofb@posteo.de>
2024-09-19 18:14:29 +0200

    Add verbose names for apps

--- users/apps.py
+++ users/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+    verbose_name = _("Users")
 
     # Instructions for importing signals:
     # https://www.geeksforgeeks.org/how-to-create-and-use-signals-in-django/
