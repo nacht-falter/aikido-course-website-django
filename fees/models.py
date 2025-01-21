c862e410e3417c007b2cafc5df76239d846c3a76
Johannes Bernet <jofb@posteo.de>
2025-01-21 22:45:07 +0100

    Refactor Fee model
    Adjust model so that fees are identifiable by course_type, fee_category,
    and fee_type. The first two are set on the course instance, the latter
    is determined dynamically on creating course registration.

--- fees/models.py
+++ fees/models.py
@@ -1,83 +1,85 @@
-from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from courses.models import InternalCourse
 from danbw_website import constants
 
 
 class Fee(models.Model):
     FEE_TYPES = [
-        ("regular", _("Regular")),
-        ("dan_seminar", _("Dan Seminar")),
-    ]
-
-    FEE_CATEGORIES = [
         ("single_session", _("Single Session")),
         ("entire_course", _("Entire Course")),
-        ("dan_prep", _("Dan Preparation Session")),
-        ("entire_course_dan_prep", _("Entire Course with Dan Preparation")),
         ("single_day", _("Single Day")),
+        ("single_session_dan_preparation", _("Dan Preparation Session")),
+        ("entire_course_dan_preparation", _("Entire Course with Dan Preparation")),
     ]
 
     course_type = models.CharField(
         _("Course Type"),
         max_length=50,
-        choices=InternalCourse.COURSE_TYPES,
+        choices=constants.COURSE_TYPES,
+    )
+    fee_category = models.CharField(
+        _("Fee Category"),
+        max_length=50,
+        choices=constants.FEE_CATEGORIES,
+        null=True,
+        blank=True,
     )
     fee_type = models.CharField(
         _("Fee Type"),
         max_length=50,
         choices=FEE_TYPES,
     )
-    fee_category = models.CharField(
-        _("Fee Category"),
-        max_length=50,
-        choices=FEE_CATEGORIES,
-        null=True,
-        blank=True,
+    amount = models.DecimalField(
+        _("Amount"),
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0)],
     )
-    dan_discount = models.BooleanField(
-        _("D.A.N. Discount"),
-        default=False
-    )
-    payment_method = models.IntegerField(
-        _("Payment method"),
-        choices=constants.PAYMENT_METHODS,
+    extra_fee_external = models.DecimalField(
+        _("Extra Fee External"),
+        max_digits=10,
+        decimal_places=2,
         default=0,
+        validators=[MinValueValidator(0)],
     )
-    amount = models.DecimalField(_("Amount"), max_digits=10, decimal_places=2)
+    extra_fee_cash = models.DecimalField(
+        _("Extra Fee Cash"),
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        validators=[MinValueValidator(0)],
+    )
 
     class Meta:
         verbose_name = _("Fee")
         verbose_name_plural = _("Fees")
-        unique_together = ['course_type', 'fee_type',
-                           'fee_category', 'dan_discount', 'payment_method']
-
-    def __str__(self):
-        fee_type_display = (
-            f"{self.get_fee_type_display()} - {self.get_fee_category_display()}"
-            if self.fee_category else self.get_fee_type_display()
-        )
-        discount_status = _(
-            "D.A.N. Discount") if self.dan_discount else _("No Discount")
-        return f"{self.course_type} - {fee_type_display} ({discount_status}) - {self.get_payment_method_display()}"
+        unique_together = ['course_type', 'fee_category', 'fee_type']
 
     @classmethod
-    def get_fee(cls, course_type, fee_type='regular', fee_category=None, payment_method='bank', dan_discount=False):
+    def get_fee(
+        cls,
+        course_type,
+        fee_category,
+        fee_type,
+        payment_method,
+        dan_member,
+        default=0
+    ):
+        """
+        Calculate the total fee based on the provided parameters.
+        """
         try:
             fee = cls.objects.get(
                 course_type=course_type,
-                fee_type=fee_type,
                 fee_category=fee_category,
-                payment_method=payment_method,
-                dan_discount=dan_discount
+                fee_type=fee_type,
             )
-            return fee.amount
+            return (
+                fee.amount
+                + (fee.extra_fee_cash if payment_method == 1 else 0)
+                + (fee.extra_fee_external if not dan_member else 0)
+            )
         except cls.DoesNotExist:
-            return None
-
-    def clean(self):
-        if self.fee_type == "dan_seminar" and self.fee_category not in ["single_day", "entire_course"]:
-            raise ValidationError(_("Invalid fee category for Dan Seminar."))
-        super().clean()
+            return default
