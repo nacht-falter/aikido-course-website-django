fc309e8f74aceb06d1b7ba19df74d9723aff1d2c
Johannes Bernet <jofb@posteo.de>
2025-02-28 15:30:13 +0100

    Add honeypot field to signup form

--- danbw_website/forms.py
+++ danbw_website/forms.py
@@ -1,14 +1,22 @@
 from allauth.account.forms import SignupForm
-from captcha.fields import CaptchaField 
+from captcha.fields import CaptchaField
+from django import forms
 
 
 class CustomSignupForm(SignupForm):
     captcha = CaptchaField()
 
-    field_order = ['username', 'email', 'password1', 'password2', 'captcha']
+    phone_number = forms.CharField(required=False, widget=forms.HiddenInput()) # Honeypot
+
+    field_order = ['username', 'email', 'phone_number',
+                   'password1', 'password2', 'captcha']
+
+    def clean_phone_number(self):
+        value = self.cleaned_data.get('phone_number')
+        if value:
+            raise forms.ValidationError("Bot detected!")
+        return value
 
     def save(self, request):
-
         user = super(CustomSignupForm, self).save(request)
-
         return user
