6e5fbc031104e5503aa06cd77a8980cda0db2b1f
Johannes Bernet <jofb@posteo.de>
2026-06-03 11:39:57 +0200

    Remove CSRF_TRUSTED_ORIGINS setting

--- danbw_website/settings.py
+++ danbw_website/settings.py
@@ -39,7 +39,6 @@ DEBUG = development
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Application definition
 
